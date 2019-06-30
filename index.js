a015e992e1db1d492194cd1b4c36f8eb4cd80c5e
cheshireCode <dac4158@gmail.com>
2019-06-30 23:53:47 +0800

    working

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 /*eslint no-console: 0 */
 
-// This script merges all package.json into a singular package.json
-
-//spawn is more complicated than exec but could handle stdout size > 200Kb by default
-// see this [issue](https://github.com/nodejs/node/issues/9829)
 // global __dirname
 
 const path = require('path');
@@ -17,28 +13,32 @@ const main = require(`${currentDir}/main`);
 //setup options
 program
   .version(pkg.version)
-  .description(
-    'Extract out package.json files in a project with multiple package.json and write to package-ejected.json'
-  )
-  .option('-r, --root <root>', 'Root folder. Default to .')
+  .description(pkg.description)
+  .usage('<input> PLACE*4,4,NORTH_MOVE_LEFT_RIGHT_REPORT')
   .option(
-    '-l, --location <location>',
-    'Packages location relative to root. Default to .'
+    '-f, --file <path>',
+    `Input file of multi-line commands such as:
+  PLACE 0,0,NORTH
+  MOVE
+  LEFT
+  RIGHT
+  REPORT
+    `,
   )
-  .option(
-    '-b, --blacklist <blacklist>',
-    'comma-separated string of keywords to filter out' +
-      'Default is empty string, meaning everything is allowed'
-  )
-  .option('-o, --out-file <outFile>', 'path to output')
   .option(
     '-d, --debug',
-    'Debug flag to print out helpful messages during runtime.'
+    'Debug flag to print out helpful messages during runtime.',
   )
+  .option(
+    '-s, --store',
+    'Create a checksum of input file and store config there to continue next time.',
+  )
+  .option('--reset', 'Ignore stored config and start anew.')
   .parse(process.argv);
 
 const options = program.opts();
 //pass along options as-is, processing should happen in main
 main({
-  ...options
+  ...options,
+  args: program.args,
 });
--- main.js
+++ main.js
@@ -1,83 +1,343 @@
 /*eslint no-console: 0 */
+'use strict';
 const { spawn } = require('child_process');
 const fs = require('fs');
-const { flattenPackages } = require('./lib');
-//readFileSync will throw error if file doesn't exist at
-//specified path, default value to be returned. "" is the bare minimum
-//for JSON.parse() to not fail
-const readFileSyncIfExist = (p, defaultValue = '""') =>
-  fs.existsSync(p) ? fs.readFileSync(p, 'utf8') : defaultValue;
+const readline = require('readline');
+const crypto = require('crypto');
 
-module.exports = ({
-  root = '.',
-  location = '.',
-  defaultProps = 'dependencies,devDependencies',
-  props = '',
-  blacklist = '',
-  packageFileName = 'package.json',
-  outFile,
-  debug: isDebug,
-  isSilent = false,
-  callback
-}) => {
-  const startingDirectory = process.cwd();
-  isDebug && console.log(`Starting directory: ${startingDirectory}`);
-  try {
-    process.chdir(root);
-    isDebug && console.log(`New directory: ${process.cwd()}`);
-  } catch (err) {
-    console.error(`chdir: ${err}`);
-  }
-  //recursively find all package.json files
-  const findAllPackages = spawn(
-    'find',
-    [location, '-name', `"${packageFileName}"`],
-    {
-      shell: true
-    }
-  );
-  findAllPackages.stdout.on('data', stdout => {
-    /* a list with full path file names, split by EoL then skip the empty strings */
-    const packageFiles = `${stdout}`.split('\n').filter(x => x);
-    if (!fs.existsSync(packageFileName)) {
-      isDebug &&
-        console.warn(`No ${packageFileName} at root location ${root}...`);
-    }
-    const main = JSON.parse(readFileSyncIfExist(packageFileName));
-    const packages = [
-      ...packageFiles.map(p => JSON.parse(readFileSyncIfExist(p))),
-      main
-    ];
-
-    const finalPackage = flattenPackages({
-      main,
-      packages,
-      props: `${props},${defaultProps}`.split(',').filter(x => x),
-      blacklist: `${blacklist}`.split(',').filter(x => x)
-    });
-
-    /* eslint-enable no-unused-vars */
-    if (outFile) {
-      //finally, write to another file
-      const content = JSON.stringify(finalPackage, null, 2);
-      fs.writeFile(outFile, content, 'utf8', function(err) {
-        if (err) {
-          return console.error(err);
-        }
-
-        console.log(`Saved to ${outFile}`);
-      });
-    } else {
-      !isSilent && console.log(finalPackage);
-    }
-    callback && callback(finalPackage);
-
-    return finalPackage;
+const processLineByLine = (filePath, lineCallback, closeCallback) => {
+  const rl = readline.createInterface({
+    input: fs.createReadStream(filePath),
+    crlfDelay: Infinity,
   });
 
-  findAllPackages.on('error', stderr => console.log(`error - ${stderr}`));
-  findAllPackages.stderr.on('data', stderr =>
-    console.log(`stderr - ${stderr}`)
-  );
-  findAllPackages.on('close', code => process.exit(code));
+  rl.on('line', lineCallback);
+  rl.on('close', closeCallback);
+};
+const DIRECTION_NORTH = 'NORTH';
+const DIRECTION_SOUTH = 'SOUTH';
+const DIRECTION_EAST = 'EAST';
+const DIRECTION_WEST = 'WEST';
+class Robot {
+  constructor(params = {}) {
+    // lazy dumping of all parameters from constructor to
+    // current instance. Defensive coding could come later
+    // (picking/omitting certain keys e.g. class methods or only allow primitive values to be passed in)
+    Object.assign(
+      this,
+      {
+        // x and y are co-ordinates, starting at 0,0
+        x: 0,
+        y: 0,
+        // grid size is 5x5 by default, but override-able
+        maxX: 5,
+        maxY: 5,
+        // check if placement has already happened, or else
+        // no movement could take place
+        isPlaced: false,
+        // stores all possible directions and used to
+        // rotate a direction to its left or right
+        //     N
+        // W       E
+        //     S
+        directions: [
+          DIRECTION_NORTH,
+          DIRECTION_WEST,
+          DIRECTION_SOUTH,
+          DIRECTION_EAST,
+        ],
+        // count number of processed lines so next time, we could skip them
+        linesToSkip: 0,
+        start: 0,
+      },
+      params,
+    );
+
+    // alas, no direct arrow function in Node without transpiling so
+    // here we manually bind class methods' "this" to the class itself to avoid
+    // using class method as callback and having "this" be something else
+    'setCoords,process,isValidCoords,isValidDirection,save'
+      .split(',')
+      .map(s => (this[s] = this[s].bind(this)));
+  }
+  /**
+   * Set co-ordinates by coercing to Int
+   * @param {Number} x
+   * @param {Number} y
+   */
+  setCoords(x, y) {
+    this.x = parseInt(x, 10);
+    this.y = parseInt(y, 10);
+  }
+  /**
+   * check if given co-ordinates are valid - positive ints that aren't beyond the grid boundary
+   * @param {Number} x
+   * @param {Number} y
+   * @returns Boolean
+   */
+  isValidCoords(x, y) {
+    return x >= 0 && y >= 0 && x <= this.maxX && y <= this.maxY;
+  }
+  /**
+   * check if given direction is valid, as one of the default values
+   *
+   * @param {String} d
+   * @returns Boolean
+   */
+  isValidDirection(d) {
+    return this.directions.indexOf(d) >= 0;
+  }
+
+  async save(cb) {
+    if (this.savePath) {
+      /* istanbul ignore next */
+      this.isDebug &&
+        console.log(
+          'Saving current config',
+          {
+            x: this.x,
+            y: this.y,
+            direction: this.direction,
+            linesToSkip: this.linesToSkip,
+          },
+          'to',
+          this.savePath,
+        );
+      await new Promise((resolve, reject) =>
+        fs.writeFile(
+          this.savePath,
+          JSON.stringify({
+            x: this.x,
+            y: this.y,
+            direction: this.direction,
+            linesToSkip: this.linesToSkip,
+          }),
+          'utf-8',
+          err => (err ? reject(err) : resolve()),
+        ),
+      );
+    }
+    if (cb) {
+      cb();
+    }
+  }
+  /**
+   * Process an instruction
+   *
+   * @param {String} str
+   * @returns Boolean|Object whether an instruction was successfully processed
+   * or rejected due to invalid inputs
+   */
+  async process(str) {
+    this.start++;
+    if (this.start <= this.linesToSkip) {
+      console.log(`${this.start} / ${this.linesToSkip} lines skipped.`);
+      return;
+    }
+    this.linesToSkip++;
+    const cmd = str.split(' ')[0];
+    const i = this.directions.indexOf(this.direction);
+    const d = this.direction;
+    // strict equality is simplest
+    // more lenient/flexible handling could always be easily added later
+    switch (cmd) {
+      case 'PLACE':
+        const rest = str
+          .substr(cmd.length)
+          .trim()
+          .split(',');
+
+        const [x, y, direction] = rest;
+        if (this.isValidCoords(x, y) && this.isValidDirection(direction)) {
+          this.setCoords(x, y);
+          this.direction = direction;
+          /* istanbul ignore next */
+          this.isDebug &&
+            console.log(
+              str,
+              `Placed. Currently at ${this.x}, ${this.y}, ${this.direction}.`,
+            );
+          return true;
+        }
+        /* istanbul ignore next */
+        this.isDebug &&
+          console.log(
+            str,
+            `Unable to place. Co-ordinates (${x}, ${y}) are ${
+              this.isValidCoords(x, y) ? '' : 'in'
+            }valid. Direction ${direction} is ${
+              this.isValidDirection(direction) ? '' : 'in'
+            }valid.`,
+          );
+        return false;
+        break;
+      case 'MOVE':
+        let isMovePossible = false;
+        if (
+          (this.direction === DIRECTION_NORTH ||
+            this.direction === DIRECTION_SOUTH) &&
+          this.isValidCoords(this.x, this.y + 1)
+        ) {
+          this.y++;
+          isMovePossible = true;
+        }
+        if (
+          (this.direction === DIRECTION_EAST ||
+            this.direction === DIRECTION_WEST) &&
+          this.isValidCoords(this.x + 1, this.y)
+        ) {
+          this.x++;
+          isMovePossible = true;
+        }
+        /* istanbul ignore next */
+        this.isDebug &&
+          console.log(
+            str,
+            `${
+              isMovePossible
+                ? 'Successful'
+                : 'Unable to move, would go out of bounds'
+            }. Currently at ${this.x}, ${this.y}, ${this.direction}.`,
+          );
+        return isMovePossible;
+        break;
+      case 'LEFT':
+        // add 1 to current index to find the rotated direction
+        // North -> West -> South -> East -> North
+        // if increasing beyond array length, reset back to 0
+        this.direction = this.directions[
+          i + 1 > this.directions.length - 1 ? 0 : i + 1
+        ];
+        this.isDebug &&
+          console.log(str, `Rotated from ${d} to ${this.direction}.`);
+        return true;
+        break;
+      case 'RIGHT':
+        // subtract 1 to current index to find the rotated direction
+        // North -> West -> South -> East -> North
+        // if decreasing beyond array length, reset back to the last element
+        this.direction = this.directions[
+          i - 1 < 0 ? this.directions.length - 1 : i - 1
+        ];
+        this.isDebug &&
+          console.log(str, `Rotated from ${d} to ${this.direction}.`);
+        return true;
+        break;
+      case 'REPORT':
+        /* istanbul ignore next */
+        this.isDebug && console.log(str, this.x, this.y, this.direction);
+        return { x: this.x, y: this.y, direction: this.direction };
+        break;
+      default:
+        /* istanbul ignore next */
+        this.isDebug && console.log(str);
+        break;
+    }
+  }
+}
+module.exports.Robot = Robot;
+module.exports = async ({
+  // root = '.',
+  debug: isDebug = true,
+  store = true,
+  file,
+  args = [],
+  separator = '_',
+  reset = false,
+  ...rest
+}) => {
+  if (!file) {
+    // console.log(args[0].replace('*', ' ').split(separator));
+    //unfortunately, CSV format wouldn't work due to PLACE X,Y,F syntax.
+    //handling that would take too much effort and not really adding value
+    //so forcing '_' is enough for now
+    if (args[0] && separator === '_') {
+      file = `/tmp/${crypto.randomBytes(20).toString('hex')}.txt`;
+      await new Promise((resolve, reject) =>
+        fs.writeFile(
+          file,
+          args[0]
+            .replace('*', ' ')
+            .split(separator)
+            .join('\n'),
+          'utf-8',
+          err => (err ? reject(err) : resolve()),
+        ),
+      );
+      /* istanbul ignore next */
+      isDebug &&
+        console.log(`Written output to temp file ${file} for processing`);
+    } else {
+      isDebug && console.log(args[0], `Unable to process empty input.`);
+    }
+  }
+  /* istanbul ignore next */
+  if (!file) {
+    console.error('No data nor input file. Unable to proceed');
+  } else {
+    /* istanbul ignore next */
+    isDebug && console.log(`Input file, ${file}`);
+    const fileCheckSum =
+      !!store &&
+      (await new Promise((resolve, reject) =>
+        fs.readFile(file, 'utf-8', (err, data) => {
+          if (err) reject(err);
+          else
+            resolve(
+              crypto
+                .createHash('sha1')
+                .update(data, 'utf8')
+                .digest('hex'),
+            );
+        }),
+      ));
+    /* istanbul ignore next */
+    isDebug && console.log('File checksum', fileCheckSum);
+    const savePath = fileCheckSum ? `/tmp/toy-robot-${fileCheckSum}` : '';
+    /* istanbul ignore next */
+    isDebug && console.log(`Processing line by line...`);
+    const prevConfig =
+      !reset && store && fs.existsSync(savePath)
+        ? await new Promise((resolve, reject) =>
+            fs.readFile(savePath, 'utf-8', (err, data) => {
+              if (err) reject(err);
+              else resolve(JSON.parse(data));
+            }),
+          )
+        : {};
+    /* istanbul ignore next */
+    isDebug && console.log('Previous config', prevConfig);
+    const r = new Robot({
+      ...prevConfig,
+      isDebug,
+      savePath,
+    });
+    processLineByLine(file, r.process, r.save);
+
+    // catching all possible signals and do something before exit (not tested yet)
+    [
+      'SIGHUP',
+      'SIGINT',
+      'SIGQUIT',
+      'SIGILL',
+      'SIGTRAP',
+      'SIGABRT',
+      'SIGBUS',
+      'SIGFPE',
+      'SIGUSR1',
+      'SIGSEGV',
+      'SIGUSR2',
+      'SIGTERM',
+    ].forEach(function(sig) {
+      process.on(sig, sig => {
+        if (typeof sig === 'string') {
+          r.save(() => {
+            /* istanbul ignore next */
+            isDebug && console.log('Exiting...');
+            process.exit(1);
+          });
+        }
+      });
+    });
+  }
 };
